445d58da00eeefdcd7bef6c011e1522ca683acc3
Zach Zhao <zachzhao@startupshell.org>
2023-05-12 21:06:55 -0400

    remove build

--- build/assets/js/1df93b7f.5613a6f3.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunk=self.webpackChunk||[]).push([[237],{3808:(e,t,u)=>{u.r(t),u.d(t,{default:()=>c});var a=u(7294);u(9960),u(2263);var r=u(6550);function c(){return a.createElement(r.l_,{to:"/docs/about/"})}}}]);
--- build/assets/js/814f3328.c9affe22.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunk=self.webpackChunk||[]).push([[535],{5641:e=>{e.exports=JSON.parse('{"title":"Recent posts","items":[{"title":"Launch!","permalink":"/blog/first-blog-post"}]}')}}]);
